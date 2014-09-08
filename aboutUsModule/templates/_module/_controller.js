412e1c36f0e846637c540c8a3f5a9278e2cb4289
Jose Figueruela <jose.figueruela@codebusters.es>
2014-09-08 16:12:10 +0200

    Prebuilt contact module

--- aboutUsModule/templates/_module/_controller.js
+++ aboutUsModule/templates/_module/_controller.js
@@ -3,8 +3,8 @@ define([
   'angular',
   './<%= routeFile %>'
 ], function(angular) {
-  angular.module('<%= angularModuleName %>', ['<%= angularModuleName %>.routing'])
-          .controller('<%= moduleControllerClass %>', ['$scope', '$log',
+  angular.module('<%=angularModuleName%>', ['<%=angularModuleName%>.routing'])
+          .controller('<%=moduleControllerClass%>', ['$scope', '$log',
     function($scope, $log) {
       
     }]);
---
+++ contactModule/templates/_module/_templates/_contact.php
@@ -0,0 +1,45 @@
+<?php
+/*error_reporting(E_ALL);
+ini_set('display_errors', '1');
+require_once 'js/PHPMailerAutoload.php';*/
+
+ini_set('display_errors', 'On');
+error_reporting(E_ALL | E_STRICT);
+
+$data = file_get_contents("php://input");
+$postData = json_decode($data, true);
+
+if (isset($postData['name']) && isset($postData['email'])  && isset($postData['text'])) {
+
+    //check if any of the inputs are empty
+    if (empty($postData['name']) || empty($postData['email']) || empty($postData['text'])) {
+        $data = array('success' => false, 'message' => 'Please fill out the form completely.');
+        echo json_encode($data);
+        exit;
+    }
+
+    $email = trim($postData['email']);
+
+    $subject = "Web message subject";
+    //email address settings
+    $headers = "From: " .$email;
+    $message = "Name: " . $postData['name'] . "\r\n\r\nMessage: " . stripslashes($postData['text']);
+    $to = "<%=contact.email%>";
+
+    if (isset($postData['ref'])) {
+        $mail->Body .= "\r\n\r\nRef: " . $postData['ref'];
+    }
+
+    mail($to, $subject, $message, $headers);
+    $data = array('success' => true, 'message' => 'Thanks! We have received your message.');
+    echo json_encode($data);
+
+
+} else {
+
+    $data = array('success' => false, 'message' => 'Please fill out the form completely.');
+    echo json_encode($data);
+
+}
+
+?>
