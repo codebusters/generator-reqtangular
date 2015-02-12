22684ed2f0c3b0071c1e776fd2288a1674042f51
Jose Figueruela <jose.figueruela@codebusters.es>
2015-02-12 18:44:44 +0100

    Fix yeoman message typo

--- app/index.js
+++ app/index.js
@@ -21,7 +21,7 @@ var NgRequireGenerator = yeoman.generators.Base.extend({
         callback: function() {
           console.log(chalk.yellow('┌────────────────────────────────────────────────────────────────┐'));
           console.log(chalk.yellow('│ ' + chalk.white('Everything is ready!, just run: ') + chalk.blue(' grunt serve')) + chalk.yellow('                   │'));
-          console.log(chalk.yellow('│ ' + chalk.white('or build distribution version with: ') + chalk.blue(' grunt serve')) + chalk.yellow('               │'));
+          console.log(chalk.yellow('│ ' + chalk.white('or build distribution version with: ') + chalk.blue(' grunt dist')) + chalk.yellow('                │'));
           console.log(chalk.yellow('│ ' + chalk.white('wiki') + chalk.blue(' https://github.com/codebusters/generator-reqtangular/wiki')) + chalk.yellow(' │'));
           console.log(chalk.yellow('└────────────────────────────────────────────────────────────────┘'));
         }
